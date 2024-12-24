fbac10730b1f3c1b33fba55a465e28afc606baf2
Kesinh0 <simon.reyes2401@alumnos.ubiobio.cl>
2024-12-24 15:16:52 -0300

    Create pokemon.h

---
+++ struct/pokemon.h
@@ -0,0 +1,21 @@
+typedef struct {
+    int id;
+    char name[15];
+    char type1[10];
+    char type2[10]; 
+    char habitat[20];
+    int evo_1;
+    int evo_2;
+    int evo_3;
+    char desc[200];
+} PKMN;
+
+typedef struct {
+	int id;
+	int HP;
+	int Atk;
+	int Def;
+	int SpA;
+	int SpD;
+	int Spe;
+} DAT;
